0fc9826d4eae8602c16c6864c094ab36f05d3f9c
Lucian Zamfira <zamfira.lucian@gmail.com>
2021-11-04 22:48:18 +0200

    js: remove logs

--- js/theme.js
+++ js/theme.js
@@ -198,10 +198,8 @@ $(document).ready(function () {
 	// Select all functionality
 	$('.button--all').on('click', function () {
 		if ($(this).hasClass('toggled')) {
-			console.log('unckeck inputs');
 			$(this).parents('ul').find('input').prop('checked', false);
 		} else {
-			console.log('check inputs');
 			$(this).parents('ul').find('input').prop('checked', true);
 		}
 
